2231fcf05d612a087a88223df9fd97599d86985a
Wilhelm Hansson <wilha431@student.liu.se>
2019-06-08 21:49:27 +0200

    Done with E1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,24 +7,26 @@ const Header = (props) => {
     )
 }
 
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercises}
+        </p>
+    )
+}
+
 const Content = (props) => {
     return (
         <>
-            <p>
-                {props.part1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
         </>
     )
 }
 
 const Total = (props) => {
-    return(
+    return (
         <p>Number of exercises {props.total}</p>
     )
 }
@@ -41,7 +43,7 @@ const App = () => {
     return (
         <>
             <Header course={course} />
-            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
             <Total total={exercises1 + exercises2 + exercises3} />
         </>
     )
