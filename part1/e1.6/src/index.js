606910761aa09523e92f873a833bacd93875966f
Wilhelm Hansson <wilha431@student.liu.se>
2019-06-17 16:52:35 +0200

    Done E1.6

--- part1/e1.6/src/index.js
+++ part1/e1.6/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Display = ({text, data}) => <p>{text}: {data}</p>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+    <h1>Give feedback</h1>
+    <Button onClick={()=> setGood(good + 1)} text="Good" />
+    <Button onClick={()=> setNeutral(neutral + 1)} text="Neutral" />
+    <Button onClick={()=> setBad(bad + 1)} text="Bad" />
+
+    <h2>Statistics</h2>
+    <Display text="Good" data={good}/>
+    <Display text="Neutral" data={neutral}/>
+    <Display text="Bad" data={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
