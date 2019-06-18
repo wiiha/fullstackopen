f285ab711b4be931221bb64f035c3ccf89a915df
Wilhelm Hansson <wilha431@student.liu.se>
2019-06-18 11:09:56 +0200

    Done E1.14

--- part1/e1.12-e1.14/src/index.js
+++ part1/e1.12-e1.14/src/index.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Display = ({heading, content, votes}) =>(
+    <>
+    <h2>{heading}</h2>
+    {content}
+    <br/>
+    {"has " + votes + " votes"}
+    </>
+)
 
 const App = (props) => {
     const [selected, setSelected] = useState(0)
+    const [mostVotes, setMostVotes] = useState(0)
     const numOfAnecdotes = anecdotes.length
     const [votes, setVotes] = useState(Array.apply(null, new Array(numOfAnecdotes)).map(Number.prototype.valueOf, 0))
 
@@ -16,17 +25,22 @@ const App = (props) => {
     const vote = () => {
         const newVotes = [ ...votes ]
         newVotes[selected] += 1
+        for (let index = 0; index < newVotes.length; index++) {
+            if (newVotes[index] > newVotes[mostVotes]) {
+                setMostVotes(index)
+            }
+        }
         setVotes(newVotes)
     }
 
     return (
         <div>
-            {props.anecdotes[selected]}
-            <br/>
-            {"has " + votes[selected] + " votes"}
+            <Display heading="Anecdote of the day" content={props.anecdotes[selected]} votes={votes[selected]}/>
             <br />
             <Button onClick={() => vote()} text="Vote" />
             <Button onClick={() => newAnecdote()} text="Next anecdote" />
+            <Display heading="Anecdote with most votes" content={props.anecdotes[mostVotes]} votes={votes[mostVotes]}/>
+
         </div>
     )
 }
