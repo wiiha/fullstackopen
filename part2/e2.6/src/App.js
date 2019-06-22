85f751b4a15c496474c14b2f27ae846f88c1e5c6
Wilhelm Hansson <wilha431@student.liu.se>
2019-06-22 20:51:20 +0200

    Done E2.8

--- part2/e2.6/src/App.js
+++ part2/e2.6/src/App.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const contactRows = () => persons.map(person => <p key={person.name}>{person.name}</p>)
+  const contactRows = () => persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
 
   const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -19,11 +21,13 @@ const App = () => {
     } else {
       const personObj = {
         name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(personObj))
     }
     setNewName('')
-
+    setNewNumber('')
+    
   }
 
   return (
@@ -32,6 +36,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
