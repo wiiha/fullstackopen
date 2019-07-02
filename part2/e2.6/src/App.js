b300ac5323ca834bf0436ac7c4a709c126f8106d
Wilhelm Hansson <wilha431@student.liu.se>
2019-07-02 18:47:24 +0200

    Done E2.9

--- part2/e2.6/src/App.js
+++ part2/e2.6/src/App.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react'
 
+const Person = ({ name, number }) => (
+  <p>{name} {number}</p>
+)
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchFilter, setSearchFilter] = useState('')
 
-  const contactRows = () => persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+  const personsToShow = (searchFilter === '')
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().search(searchFilter.toLowerCase()) > -1)
+
+  const contactRows = () => personsToShow.map(person =>
+    <Person
+      key={person.name}
+      name={person.name}
+      number={person.number}
+    />)
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
@@ -27,12 +44,20 @@ const App = () => {
     }
     setNewName('')
     setNewNumber('')
-    
+
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      Search by name: <input value={searchFilter} onChange={handleSearchChange} />
+      <div>Debug: {searchFilter}</div>
+
+      <h3>Add new person</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -44,6 +69,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       {contactRows()}
     </div>
